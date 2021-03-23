7a9707044e6752ae64865594000ffe23e35f7f97
linuxlite <ariaharper0@gmail.com>
2021-03-22 21:08:18 -0400

    Update CODE/CMakeLists.txt

--- CODE/Default/cmake_install.cmake
+++ CODE/Default/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/linuxlite/shared/albot-code
+# Install script for directory: /home/linuxlite/shared/albot-cpp/CODE/Default
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,6 +37,12 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/linuxlite/shared/albot-cpp/CODE/Default/IXWebSocket/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/linuxlite/shared/albot-code/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/linuxlite/shared/albot-cpp/CODE/Default/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
